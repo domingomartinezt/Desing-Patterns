ffcd629e2c50bff9061d84a2f0ef0e427539b224
Domingo Javier Martinez Torrivilla <Domingo_Martinez@Dell.com>
2019-10-30 22:23:00 -0500

    Add project files.

---
+++ Desing-Patterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Desing_Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
